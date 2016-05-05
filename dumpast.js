0522c2b8e12f5a54e0281333b0f0cf778ed00a35
Ewan Dennis <ewandennis@users.noreply.github.com>
2016-05-05 12:02:29 +0100

    dumpAST comment cleanup

--- dumpast.js
+++ dumpast.js
@@ -5,8 +5,6 @@ console.log(
   JSON.stringify(
     handlebars.parse(
       fs.readFileSync(process.argv[2], 'utf8')
-      //process.argv[3]
-      //"{{#if x}}x{{#if xx}}xx{{else if y}}yy{{/if}}{{else}}y{{/if}}"
     ), null, '  '
   )
 );
